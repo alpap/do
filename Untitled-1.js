bd8b1f64fafe5593f82a0a98d1468bbc8030c792
Alexandros Papadopoulos <al.pap@live.com>
2024-04-18 19:48:03 +0200

    D  Untitled-1.js A  mocks/file.json

--- Untitled-1.js
+++
@@ -1,8 +0,0 @@
-function getDepth(o) {
-  return Object(o) === o ? 1 + Math.max(-1, ...Object.values(o).map(getDepth)) : 0
-}
-
-const s = {q: 'eqwee', w: 'weqe'}
-
-const sss = getDepth(s)
-console.log(sss)
